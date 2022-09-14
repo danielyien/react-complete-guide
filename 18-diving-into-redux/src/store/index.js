6c4a2b951d2c22547fd9294bfa4b9c4a2163cbb6
Daniel Yien Han Wu <danielyhw@tcu.gov.br>
2022-09-14 20:03:59 -0300

    React

---
+++ 18-diving-into-redux/src/store/index.js
@@ -0,0 +1,49 @@
+import { createStore, combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import counterReducer from './counter'
+import authReducer from './auth'
+
+// const counterReducer = (state = initialCounterState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         };
+//     }
+//
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         };
+//     }
+//
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         };
+//     }
+//
+//     if (action.type === 'toggle') {
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter
+//         };
+//     }
+//
+//     return state;
+// }
+
+// const store = createStore(counterReducer);
+// const store = createStore(counterSlice.reducer);
+// const store = configureStore({
+//     reducer: counterSlice.reducer
+// });
+
+const store = configureStore({
+    reducer: {counter: counterReducer, auth: authReducer}
+});
+
+export default store;
